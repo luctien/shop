3c58cf51b571b1078e590cd6b96c0f955d767362
luctien <tienluc888@gmail.com>
2023-04-26 20:43:02 +0700

    update

--- assets/main.js
+++ assets/main.js
@@ -1,80 +1,57 @@
 // import owl js
-// $(document).ready(function(){
-//   $('.owl-carousel').owlCarousel({
-//     loop:true,
-//     margin:0,
-//     nav:true,
-//     autoplay:false,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:2
-//         },
-//         1000:{
-//             items:4
-//         }
-//     }
-// })
-// });
-
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-      loop:true,
-      margin:10,
-      nav:true,
-      autoplay:false,
-      autoplayTimeout:3000,
-      autoplayHoverPause:true,
-      center: false,
-      navText: [
-          "<i class='fa fa-angle-left'></i>",
-          "<i class='fa fa-angle-right'></i>"
-      ],
-      responsive:{
-          0:{
-              items:1
-          },
-          600:{
-              items:1
-          },
-          1000:{
-              items:4
-          }
-      }
-    });
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    center: false,
+    navText: [
+      "<i class='fa fa-angle-left'></i>",
+      "<i class='fa fa-angle-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      600: {
+        items: 2,
+      },
+      1000: {
+        items: 4,
+      },
+    },
   });
+});
 
-// Slick
-  $(document).ready(function () {
-    $(".image-slider").slick({
-      slidesToShow: 4,
-      slidesToScroll: 1,
-      infinite: true,
-      arrows: true,
-      draggable: true,
-      prevArrow: `<button type='button' class='slick-prev slick-arrow'><ion-icon name="arrow-back-outline"></ion-icon></button>`,
-      nextArrow: `<button type='button' class='slick-next slick-arrow'><ion-icon name="arrow-forward-outline"></ion-icon></button>`,
-      dots: true,
-      responsive: [
-        {
-          breakpoint: 1025,
-          settings: {
-            slidesToShow: 3,
-          },
+// card-slider
+$(document).ready(function () {
+  $(".card-slider").slick({
+    dots: false,
+    arrows: true,
+    slidesToShow: 4,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
         },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            arrows: false,
-            infinite: false,
-          },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
         },
-      ],
-      // autoplay: true,
-      // autoplaySpeed: 1000,
-    });
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
-  
+});
