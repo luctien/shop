877df61e2cd3348eb74c4a9cc1e7207916db7d8d
luctien <tienluc888@gmail.com>
2023-05-09 08:46:49 +0700

    repon

--- assets/main.js
+++ assets/main.js
@@ -59,3 +59,13 @@ $(document).ready(function () {
     ],
   });
 });
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)"
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.body.style.backgroundColor = "white";
+}
